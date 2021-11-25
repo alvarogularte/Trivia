e0463fd057c48507143e638acb93b9a5ef676731
Augusto Alves <augusto.malves@yahoo.com.br>
2021-11-24 23:26:02 -0300

    add require 3 and 4

---
+++ src/pages/Conf.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Conf extends React.Component {
+  render() {
+    return <h1 data-testid="settings-title">Configurações</h1>;
+  }
+}
+
+export default Conf;
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from '../components/Header';
 
 class Main extends React.Component {
   render() {
     return (
-      <div />
+      <div>
+        <Header />
+      </div>
     );
   }
 }
